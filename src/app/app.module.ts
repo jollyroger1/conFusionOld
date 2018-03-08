8a82ef488e46707b8998a6c7629773081d3af529
Roger Adelmann <roger.adelmann@bnymellon.com>
2018-03-07 18:45:50 -0600

    Second Commit - Adding Menu Bar and Scaffold Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs'; 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
-
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -13,6 +16,11 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	HttpModule,
+	MaterialModule,
+	FlexLayoutModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
